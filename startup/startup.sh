f30050252673b8049b3fb463cf728abf7b07b9ed
40288378 <bsweeney08@qub.ac.uk>
2021-02-23 18:02:03 +0000

    remove docker-compose file

--- startup/startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo docker-compose up
-
-SERVER_IP_ADDRESS=$( sudo docker inspect --format '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' my-freeipa-container)
-
-echo "$SERVER_IP_ADDRESS  ipa.xyz.test" | sudo tee -a /etc/hosts
