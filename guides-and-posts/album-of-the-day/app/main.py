0e7fb845b2a3aef34449181248fdf5acd3cdfdd5
40288378 <bsweeney08@qub.ac.uk>
2021-03-04 22:00:00 +0000

    finish first draft of updated guide

--- guides-and-posts/album-of-the-day/app/main.py
+++ guides-and-posts/album-of-the-day/app/main.py
@@ -8,14 +8,14 @@ import requests
 
 # Start Selenium webdriver container
 os.system(
-    'sudo docker run --name my-selenium-container -d -p 4444:4444 -p 5900:5900 -v /dev/shm:/dev/shm selenium/standalone-firefox:4.0.0-beta-1-prerelease-20210210')
+    'sudo docker run --name my-selenium-container -d -p 4444:4444  -v /dev/shm:/dev/shm selenium/standalone-firefox:4.0.0-beta-1-prerelease-20210210')
 
 # Wait for Selenium webdriver container to be ready
 ready = False
 time.sleep(5)
 while not ready:
     try:
-        r = requests.get('http://172.17.0.1:4444/wd/hub/status', timeout=1)
+        r = requests.get('http://localhost:4444/wd/hub/status', timeout=1)
         status = r.status_code
         if status == 200:
             ready = True
@@ -23,13 +23,14 @@ while not ready:
         continue
 
 driver = webdriver.Remote(desired_capabilities=webdriver.DesiredCapabilities.FIREFOX,
-                          command_executor="http://172.17.0.1:4444/wd/hub")
+                          command_executor="http://localhost:4444/wd/hub")
 
 month_name = 'june'
 month_code = '06'
 month_name_short = 'Jun'
 year = '1999'
 day_of_month = '1'
+release_date_to_search = '%s %s' % (month_name_short, day_of_month)
 
 driver.get("https://www.albumoftheyear.org/%s/releases/%s-%s.php?s=release&genre=all" % (year, month_name, month_code))
 
@@ -37,21 +38,25 @@ all_albums_loaded = False
 
 while not all_albums_loaded:
     try:
-        show_more_button = driver.find_element_by_class_name('showMore')
+        show_more_button_container = driver.find_element_by_class_name('showMore')
         time.sleep(3)
-        driver.execute_script("arguments[0].click();", show_more_button.find_element_by_class_name('largeButton'))
+        driver.execute_script("arguments[0].click();",
+                              show_more_button_container.find_element_by_class_name('largeButton'))
         time.sleep(1)
     except StaleElementReferenceException:
         continue
-    except NoSuchElementException or ElementClickInterceptedException:
+    except NoSuchElementException:
         all_albums_loaded = True
 
 albums = driver.find_elements_by_class_name('albumBlock')
 
 for album in albums:
-    if album.find_element_by_class_name('date').text == '%s %s' % (month_name_short, day_of_month):
-        print(album.find_element_by_class_name('albumTitle').text + ' - ' + album.find_element_by_class_name(
-            'artistTitle').text)
+    album_release_date = album.find_element_by_class_name('date').text
+    if album_release_date == release_date_to_search:
+        album_title = album.find_element_by_class_name('albumTitle').text
+        artist_title = album.find_element_by_class_name(
+            'artistTitle').text
+        print('%s - %s' % (album_title, artist_title))
 
 driver.close()
 
