be16d8736b524794ec000ca1fbca729fb5e40061
40288378 <bsweeney08@qub.ac.uk>
2021-04-15 15:52:07 +0100

    finish first draft of guide

--- developer/guides-and-posts/social-media-post/guide-pt-2/aoty-backend-flask/main.py
+++ developer/guides-and-posts/social-media-post/guide-pt-2/aoty-backend-flask/main.py
@@ -1,11 +1,15 @@
+# For handling requests and responses
+import requests
+import json
 from flask import Flask, request
+# Selenium imports
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, \
     StaleElementReferenceException
 import time
+# To run Docker commands
 import os
-import requests
-import json
+# To enable requests from the frontend user interface
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
@@ -18,22 +22,11 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 os.system(
     'sudo docker run --name my-selenium-container -d -p 4444:4444  -v /dev/shm:/dev/shm selenium/standalone-firefox:4.0.0-beta-1-prerelease-20210210')
 
-# Wait for Selenium webdriver container to be ready
-ready = False
-time.sleep(5)
-while not ready:
-    try:
-        r = requests.get('http://localhost:4444/wd/hub/status', timeout=1)
-        status = r.status_code
-        if status == 200:
-            ready = True
-    except ConnectionResetError or ConnectionError:
-        continue
-
 
 @app.route('/', methods=['POST'])
 @cross_origin()
-def hello_world():
+def search_albums():
+    # Get data from form
     data = request.json
     day = data['day']
     month_code = data['month_code']
@@ -41,8 +34,7 @@ def hello_world():
     month = ''
     month_name_short = ''
 
-    print(month_code)
-
+    # Set month and month_name_short based on month_code
     if month_code == '01':
         month = 'january'
         month_name_short = 'Jan'
@@ -80,14 +72,31 @@ def hello_world():
         month = 'december'
         month_name_short = 'Dec'
 
+    # Format release date to search for like "Sep 20"
     release_date_to_search = '%s %s' % (month_name_short, day)
+
+    # Wait for Selenium webdriver container to be ready
+    ready = False
+    while not ready:
+        try:
+            r = requests.get('http://localhost:4444/wd/hub/status', timeout=1)
+            status = r.status_code
+            if status == 200:
+                ready = True
+        except ConnectionResetError or ConnectionError:
+            continue
+
+    # Connect to remote Firefox webdriver
     driver = webdriver.Remote(desired_capabilities=webdriver.DesiredCapabilities.FIREFOX,
                               command_executor="http://localhost:4444/wd/hub")
+
+    # Go to Album of the Year page for specific month
     driver.get(
         "https://www.albumoftheyear.org/%s/releases/%s-%s.php?s=release&genre=all" % (year, month, month_code))
 
     all_albums_loaded = False
 
+    # Keep clicking button to show more albums while there are still albums to load
     while not all_albums_loaded:
         try:
             show_more_button_container = driver.find_element_by_class_name('showMore')
@@ -100,9 +109,11 @@ def hello_world():
         except NoSuchElementException:
             all_albums_loaded = True
 
+    # Find elements containing albums info on page
     albums = driver.find_elements_by_class_name('albumBlock')
 
     response = []
+    # For each album, if the release date matches the date to search for then append it to the response array
     for album in albums:
         album_release_date = album.find_element_by_class_name('date').text
         if album_release_date == release_date_to_search:
@@ -111,15 +122,14 @@ def hello_world():
                 'artistTitle').text
             response.append('%s - %s' % (album_title, artist_title))
 
+    # If no albums are found return a message informing the user
     if len(response) == 0:
         response.append('No albums found')
 
+    # Close connection to Firefox webdriver
     driver.close()
-    #
-    # # remove Selenium container
-    # os.system(
-    #     'sudo docker rm -f my-selenium-container')
 
+    # Return response to user
     return json.dumps({'results': response}), 200, {'ContentType': 'application/json'}
 
 
